6361b3c0baae3e171f489fe9ecbbc9bf97b2aec9
joao97braga <joao97braga@outlook.com>
2025-02-08 16:15:55 +0000

    Button color change

--- client/src/components/AnimalCard.jsx
+++ client/src/components/AnimalCard.jsx
@@ -91,7 +91,7 @@ const AnimalCard = ({
           <span className="flex-1 py-2 text-xs font-medium text-gray-600">
             {shelterName || "Shelter Name"}
           </span>
-          <button className="flex-1 px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md hover:bg-blue-700 focus:outline-none">
+          <button className="flex-1 px-4 py-2 text-sm font-medium text-white bg-[#207CC8] border border-transparent rounded-md hover:bg-blue-700 focus:outline-none">
             Adopt Me
           </button>
         </div>
