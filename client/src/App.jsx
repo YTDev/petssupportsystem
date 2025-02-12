32fe779a856b6af3f32ae9f9d30a0dfa79ee2a1d
joao97braga <94471401+joao97braga@users.noreply.github.com>
2025-02-12 18:26:02 +0000

    Loading Skeleton fix
    fix loading skeleton and added simulated data fetching from an API

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "./App.css";
 import Navbar from "./components/Navbar";
 import AnimalCard from "./components/AnimalCard";
 
 function App() {
+
+  const [animalData, setAnimalData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  //Simulate fetching data from an API
+  useEffect(() => {
+    const fetchData = () => {
+      setTimeout(() => {
+        setAnimalData({
+          name: "Buddy",
+          age: "2 years",
+          size: "Medium",
+          description: "Buddy is a very playful and friendly dog. He loves to play fetch and go on long walks.",
+          image: "https://picsum.photos/id/237/280/180",
+          status: "Available",
+          homeDetails: "Apartment",
+          shelterName: "Cantinho do Tareco",
+        });
+        setIsLoading(false);
+      }, 1000); //1 seconds
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       {/* <Navbar /> */}
       <AnimalCard 
-      name="Buddy"
-      age="2 years"
-      size="Medium"
-      description="Buddy is a very playful and friendly dog. He loves to play fetch and go on long walks."
-      isLoading={false}
-      image="https://picsum.photos/id/237/280/180"
-      status="Available"
-      homeDetails="Apartment"
-      shelterName="Cantinho do Tareco"
+        isLoading={isLoading}
+        {...animalData}
       />
       {/* <div className="h-[1000vh] bg-gray-300"></div> */}
     </>
--- client/src/components/AnimalCardSkeleton.jsx
+++ client/src/components/AnimalCardSkeleton.jsx
@@ -1,54 +1,54 @@
 import React from "react";
 import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 import { BookmarkIcon } from "@heroicons/react/24/outline";
 import { MapPinIcon } from "@heroicons/react/24/solid";
 
 const AnimalCardSkeleton = () => {
     return (
-        <div className="w-72 rounded-lg border border-gray-200 bg-white shadow-sm overflow-hidden bg-blue-50">
-            {/* Header */}
-            <div className="p-4 border-b border-gray-200">
-                <div className="flex items-center justify-between">
-                    <div className="flex mr-4">
-                        <Skeleton containerClassName="flex-1" height={24} />
-                    </div>
-                    <div className="flex items-center gap-2">
-                        <Skeleton containerClassName="flex-1" circle width={24} height={24} />
-                        <BookmarkIcon className="size-6 text-gray-600" />
-                    </div>
-                </div>
+      <div className="w-72 rounded-lg border border-gray-200 bg-white shadow-sm overflow-hidden">
+        {/* Header */}
+        <div className="p-4 border-b border-gray-200">
+          <div className="flex items-center justify-between">
+            <div className="flex-1 mr-4">
+              <Skeleton width={120} height={24} />
             </div>
-
-            {/* Content */}
-            <div className="p-4 space-y-4">
-                <div className="relative">
-                    <Skeleton containerClassName="flex-1" height={192} borderRadius={6} />
-                </div>
-
-                <div className="space-y-2">
-                    <div className="flex gap-10">
-                        <Skeleton containerClassName="flex-1" width={80} height={20} />
-                        <Skeleton containerClassName="flex-1" width={80} height={20} />
-                    </div>
-                    <Skeleton containerClassName="flex-1" width={160} height={20} />
-                    <div className="space-y-1">
-                        <Skeleton containerClassName="flex-1" count={3} height={16} />
-                    </div>
-                </div>
+            <div className="flex items-center gap-2">
+              <Skeleton width={80} height={24} borderRadius={20} />
+              <BookmarkIcon className="size-6 text-gray-200" />
             </div>
-
-            {/* Footer */}
-            <div className="p-4 border-t border-gray-200 bg-gray-50">
-                <div className="flex gap-2 items-center">
-                    <MapPinIcon className="size-5 text-gray-600" />
-                    <Skeleton containerClassName="flex-1" width={100} height={20} />
-                    <div className="flex-1">
-                        <Skeleton containerClassName="flex-1" height={36} borderRadius={6} />
-                    </div>
-                </div>
-            </div>
+          </div>
         </div>
+  
+        {/* Content */}
+        <div className="p-4 space-y-4">
+          <div className="relative">
+            <Skeleton height={192} borderRadius={6} />
+          </div>
+          <div className="space-y-2">
+            <div className="flex gap-10">
+              <Skeleton width={80} height={20} />
+              <Skeleton width={80} height={20} />
+            </div>
+            <Skeleton width={160} height={20} />
+            <div className="space-y-1">
+              <Skeleton count={3} height={16} />
+            </div>
+          </div>
+        </div>
+  
+        {/* Footer */}
+        <div className="p-4 border-t border-gray-200 bg-gray-50">
+          <div className="flex gap-2 items-center">
+            <MapPinIcon className="size-5 text-gray-200" />
+            <Skeleton width={100} height={20} />
+            <div className="flex-1">
+              <Skeleton height={36} borderRadius={6} />
+            </div>
+          </div>
+        </div>
+      </div>
     );
-};
-
+  };
+  
 export default AnimalCardSkeleton;
