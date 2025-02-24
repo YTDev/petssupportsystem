895a7bcf2e3a637aa66e7452a92bb47c48d4f657
joao97braga <joao97braga@outlook.com>
2025-02-24 18:04:43 +0000

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,27 +8,7 @@ function App() {
 
   const [animalData, setAnimalData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-  
-  //Simulate fetching data from an API
-  // useEffect(() => {
-  //   const fetchData = () => {
-  //     setTimeout(() => {
-  //       setAnimalData({
-  //         name: "Buddy",
-  //         age: "2 years",
-  //         size: "Medium",
-  //         description: "Buddy is a very playful and friendly dog. He loves to play fetch and go on long walks.",
-  //         image: "https://mymodernmet.com/wp/wp-content/uploads/2017/09/dog-portraits-alexander-khokhlov-5.jpg",
-  //         status: "Available",
-  //         homeDetails: "Apartment",
-  //         shelterName: "Cantinho do Tareco",
-  //       });
-  //       setIsLoading(false);
-  //     }, 5000); //1 seconds
-  //   };
 
-  //   fetchData();
-  // }, []);
 
   useEffect(() => {
     const fetchData = () => {
@@ -38,7 +18,7 @@ function App() {
           age: "2 years",
           sex: "M",
           image: "https://mymodernmet.com/wp/wp-content/uploads/2017/09/dog-portraits-alexander-khokhlov-5.jpg",
-          status: "Pending",
+          status: "Foster",
         });
         setIsLoading(false);
       }, 2000); //1 seconds
@@ -50,10 +30,6 @@ function App() {
   return (
     <>
       {/* <Navbar /> */}
-      {/* <AnimalCard
-        isLoading={isLoading}
-        {...animalData}
-      /> */}
 
       <AnimalCard
         isLoading={isLoading}
