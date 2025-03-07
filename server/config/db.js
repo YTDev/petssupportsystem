6dff8e9041aeae32d9843a3bf659064b8a9e6580
joao97braga <joao97braga@outlook.com>
2025-03-07 17:38:57 +0000

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,8 @@ require("dotenv").config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
+  // password: process.env.DB_PASSWORD || "",
+  password: "",
   database: process.env.DB_NAME || "pet_adoption",
   waitForConnections: true,
   connectionLimit: 10,
