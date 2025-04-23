abd41f9c89ff012976a52c3b5b2c2195d693a410
JFernandes <152723673+JNoblefrost@users.noreply.github.com>
2025-04-23 03:37:37 +0100

    Changed the Logo and Adoption language

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -24,7 +24,7 @@ function SignUp() {
       <div className="bg-[#207CC8] bg-gradient-to-b from-[#103D62] from-0% to-[#207CC8] to-90% pb-[260px] text-[#f9e5bd]">
         <header className="flex justify-between max-w-5xl mx-auto items-center pt-8 pb-16 px-2 sm:px-7">
           <Link to="/" className="text-xl font-bold">
-            Logo
+            <img src="/OnlyPawsShadeless.svg" alt="" width="70vw" />
           </Link>
           <div>
             <span className="text-amber-300 text-xs mr-2 sm:text-sm sm:mr-4">
